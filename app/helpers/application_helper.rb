b7120a2f6fe281d8955632948031b6052569129e
Jorge Coutinho <jorgecsn@id.uff.br>
2024-04-16 16:23:03 -0300

    add gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def avatar_url(user, size: 200)
+    hash = Digest::MD5.hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{hash}.png"
+  end
 end
