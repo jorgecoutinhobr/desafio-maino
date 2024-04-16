db828c0a13e60b1b222dd03f47a1393d1b9983c6
Jorge Coutinho <jorgecsn@id.uff.br>
2024-04-16 01:47:36 -0300

    add has_many posts to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,11 +39,11 @@ class PostsController < ApplicationController
 
   private
 
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
